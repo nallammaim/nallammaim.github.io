7812078476fd4f44bd5de71801ff3cad23aac1f2
alagappan <alagappan.maruthappan@verizonmedia.com>
2020-08-08 00:55:56 -0700

    Initial commit

---
+++ precache-manifest.4b4d775c12b270b5b1857bbca398758b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c40082f18710920c9aab2094fe824db4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afaa08eba66741458601",
+    "url": "/static/css/main.60be8ec6.chunk.css"
+  },
+  {
+    "revision": "0f465bd35b9bbc118e4f",
+    "url": "/static/js/2.dc5fafa3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dc5fafa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afaa08eba66741458601",
+    "url": "/static/js/main.308d2825.chunk.js"
+  },
+  {
+    "revision": "87fef1bfb07714cb771e",
+    "url": "/static/js/runtime-main.454666c3.js"
+  },
+  {
+    "revision": "7cd92069f47b48e50dedd69f7a704558",
+    "url": "/static/media/Illustration1.7cd92069.png"
+  },
+  {
+    "revision": "dfb2ea3c2ab3a8dafafe2226ac6fb008",
+    "url": "/static/media/Illustration2.dfb2ea3c.png"
+  },
+  {
+    "revision": "fd3a054f1abfa8be8cc81359718f3ca6",
+    "url": "/static/media/Illustration3.fd3a054f.png"
+  },
+  {
+    "revision": "df92c81182058492697117d089016820",
+    "url": "/static/media/Illustration4.df92c811.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
